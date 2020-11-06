d0e267b0cec1dec6f29d3b427023fc7a961d7eeb
roberto riveros <roberto.riveros15@gmail.com>
2020-11-06 19:41:21 -0300

    initial commit

---
+++ src/main/java/com/github/tp_electiva3/TpElectiva3Application.java
@@ -0,0 +1,13 @@
+package com.github.tp_electiva3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpElectiva3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpElectiva3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/tp_electiva3/TpElectiva3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tp_electiva3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpElectiva3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
